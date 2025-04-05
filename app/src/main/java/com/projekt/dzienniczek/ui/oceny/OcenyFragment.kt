c7f9b23d453220e333902a97edaafd1f194cdd42
Cezary Ewiak <cewiak@lsisoftware.pl>
2025-04-05 21:03:02 +0200

    czyszczenie kodu

--- app/src/main/java/com/projekt/dzienniczek/ui/oceny/OcenyFragment.kt
+++ app/src/main/java/com/projekt/dzienniczek/ui/oceny/OcenyFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.projekt.dzienniczek.databinding.FragmentOcenyBinding
-import com.projekt.dzienniczek.databinding.FragmentRegistrationBinding
 
 class OcenyFragment : Fragment() {
 
